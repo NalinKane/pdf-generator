3727ba23f6654bdbfd03096e068b336ec4ccec15
Anna Lebiedziewicz <nalin.kane@gmail.com>
2019-12-01 17:58:25 +0000

    Create a startup index.js file

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const inquirer = require("inquirer");
+const { getUser } = require("./services/github");
+const { generatePdf } = require("./components/generatePdf");
+
+const questions = [
+  { name: "username", message: "What is your GitHub username?" }
+];
+
+async function askForUsername() {
+  const { username } = await inquirer.prompt(questions[0]);
+  return username;
+}
+
+async function askForColour(username) {
+  const { colour } = await inquirer.prompt([
+    {
+      name: "colour",
+      message: `What is ${username}'s favourite colour?`
+    }
+  ]);
+  return colour;
+}
+
+module.exports = {
+  init: async function() {
+    const username = await askForUsername();
+    console.log("Please wait, searching for the username...");
+    const gitHubAccount = await getUser(username);
+    const colour = await askForColour(gitHubAccount.name.split(" ")[0]);
+
+    const data = {
+      colour,
+      title: "A new Brazilian School",
+      date: "05/12/2018",
+      name: "kicia",
+      age: 28,
+      birthdate: "12/07/1990",
+      course: "Computer Science",
+      obs:
+        "Graduated in 2014 by Federal University of Lavras, work with Full-Stack development and E-commerce."
+    };
+    console.log("Please wait, generating a PDF");
+    await generatePdf(data, colour);
+    console.log(`Completed! Please open "./out/${username}.pdf" `);
+  }
+};
